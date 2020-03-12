f3ca39aaa45a902773d6e7dcb1cf0ee410ba94b0
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-12 18:42:21 +0100

    forgot commit older files, week-01 day-05

--- week-01/day-05/src/DrawDiagonal.java
+++ week-01/day-05/src/DrawDiagonal.java
@@ -15,18 +15,15 @@ public class DrawDiagonal {
 
         for (int i = 0; i < n ; i++) {
         //    System.out.println("---------");
-            for (int j = 1; j < n+1; j++) {
-                if (i == 0) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n-1 || j == 0 || j == n-1) {
                     System.out.print("%");
                 }
-                else if (i == n-1){
+                else if ( i == j){
                     System.out.print("%");
-                }
-                else if (j % 2 == 0){
-                    System.out.print(" ");
                 }
                 else {
-                    System.out.print("%");
+                    System.out.print(" ");
                 }
             }
             System.out.println();
