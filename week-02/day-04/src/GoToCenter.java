cbc9445024c288ec0ec54630e30c3a81fe08e9c9
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-12 13:36:04 +0100

    Diagonals and GoToCenter are done

---
+++ week-02/day-04/src/GoToCenter.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a function that draws a single line and takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a line from that point to the center of the canvas.
+        // Draw at least 3 lines with that function using a loop.
+        int x = 20;
+        int y = 20;
+
+        for (int i = 0; i < 3; i++) {
+            drawOneLine(x,y,graphics);
+            x+=70;
+        }
+    }
+
+    public static void drawOneLine (int x, int y, Graphics endpoint){
+
+        endpoint.setColor(Color.BLACK);
+        endpoint.drawLine(x, y,WIDTH/2, HEIGHT /2);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
