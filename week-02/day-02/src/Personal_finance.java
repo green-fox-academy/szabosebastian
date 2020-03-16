ff53712d9e2112c28680c3c69a7173da5f9e8ec0
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-16 15:53:16 +0100

    Personal_finance done

---
+++ week-02/day-02/src/Personal_finance.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Personal_finance {
+  public static void main(String[] args) {
+    int sum = 0;
+    int max = 0;
+
+    ArrayList<Integer> finance = new ArrayList<>();
+
+    finance.addAll(Arrays.asList(500, 1000, 1250, 175, 800, 120));
+
+    for (int i = 0; i < finance.size(); i++) {
+      sum += finance.get(i);
+    }
+    System.out.println("Spent money:  " + sum);
+
+    for (int j = 0; j < finance.size() ; j++) {
+      if(finance.get(j) > max) {
+        max = finance.get(j);
+      }
+    }
+    System.out.println("Greatest expense: " + max);
+
+    int min = finance.get(0);
+
+    for (int k = 0; k < finance.size(); k++) {
+      if(finance.get(k) < min) {
+        min = finance.get(k);
+      }
+    }
+    System.out.println("Cheapest spending: " + min);
+
+    System.out.println("Average amount of spending: " + (sum/finance.size()));
+  }
+}
--- week-02/day-02/src/map_introduction_2.java
+++ week-02/day-02/src/map_introduction_2.java
@@ -2,37 +2,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class map_introduction_2 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        HashMap<String, String> mysecondMap = new HashMap<>();
+    HashMap<String, String> mysecondMap = new HashMap<>();
 
-        mysecondMap.put("978-1-60309-452-8","A Letter to Jo");
-        mysecondMap.put("978-1-60309-459-7","Lupus");
-        mysecondMap.put("978-1-60309-444-3","Red Panda and Moon Bear");
-        mysecondMap.put("978-1-60309-461-0","The Lab");
+    mysecondMap.put("978-1-60309-452-8", "A Letter to Jo");
+    mysecondMap.put("978-1-60309-459-7", "Lupus");
+    mysecondMap.put("978-1-60309-444-3", "Red Panda and Moon Bear");
+    mysecondMap.put("978-1-60309-461-0", "The Lab");
 
-        for (String key: mysecondMap.keySet()) {
-            String value = mysecondMap.get(key);
-                System.out.println(value + " (ISBN: " + key + ")");
-        }
-        System.out.println();
-
-        mysecondMap.remove("978-1-60309-444-3");
-
-        String removeValue = "The Lab";
-
-        for (String value: mysecondMap.keySet()) {
-            if (removeValue.equals(mysecondMap.get(value))) {
-                mysecondMap.remove(value);
-            }
-        }
-
-        mysecondMap.put("978-1-60309-450-4","They Called Us Enemy");
-        mysecondMap.put("978-1-60309-453-5","Why Did We Trust Him?");
-
-        System.out.println(mysecondMap.get("478-0-61159-424-8"));
-        System.out.println(mysecondMap.get("978-1-60309-453-5"));
-
-        System.out.println(mysecondMap);
+    for (String key : mysecondMap.keySet()) {
+      String value = mysecondMap.get(key);
+      System.out.println(value + " (ISBN: " + key + ")");
     }
+    System.out.println();
+
+    mysecondMap.remove("978-1-60309-444-3");
+
+    String removeValue = "The Lab";
+
+    for (String value : mysecondMap.keySet()) {
+      if (removeValue.equals(mysecondMap.get(value))) {
+        mysecondMap.remove(value);
+      }
+    }
+
+    mysecondMap.put("978-1-60309-450-4", "They Called Us Enemy");
+    mysecondMap.put("978-1-60309-453-5", "Why Did We Trust Him?");
+
+    System.out.println(mysecondMap.get("478-0-61159-424-8"));
+    System.out.println(mysecondMap.get("978-1-60309-453-5"));
+
+    System.out.println(mysecondMap);
+  }
 }
