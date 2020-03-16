36d3f49abeeb5a639c8afc9e9d7c244a2036feb5
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-16 15:09:33 +0100

    Factorio done

---
+++ week-02/day-01/src/Factorio.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factorio {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Write a number");
+    int number = input.nextInt();
+
+    System.out.println(factorio(number));
+  }
+
+  public static int factorio(int fact) {
+    int result = 1;
+
+    for (int i = 1; i <= fact; i++) {
+      result = result*i;
+
+    }
+    return result;
+  }
+}
