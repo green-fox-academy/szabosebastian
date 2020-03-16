d1aeb5c8bab29bc1065f475e6dc47d84d5c52835
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-16 14:15:04 +0100

    Sum fixed

--- week-02/day-01/src/Sum.java
+++ week-02/day-01/src/Sum.java
@@ -1,25 +1,20 @@
-import java.util.Arrays;
 import java.util.Scanner;
-/*
+
 public class Sum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int i = scanner.nextInt();
-        int [] myArray = new int[i];
+        int input = scanner.nextInt();
 
-        for (int j = 0; j < myArray.length ; j++) {
-            myArray[j] = j;
+        System.out.println(sum(input));
+    }
+
+    public static int sum (int limit) {
+        int result = 0;
+
+        for (int i = 0; i <= limit; i++) {
+            result += i;
         }
-
-        System.out.println(sumFunction(myArray[]));
-    }0
-
-    public static int sumFunction (int limit) {
-        for (int i = 0; i < limit; i++) {
-            System.out.println("+" +i);
-        }
-        return limit;
+        return result;
     }
 }
-*/
