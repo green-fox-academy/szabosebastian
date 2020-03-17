b5b3d7e2d47bfc4611983f6e8ef20c2b14c3fbfa
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-17 16:26:04 +0100

    Counter done

---
+++ week-03/day-02/src/counter/Counter.java
@@ -0,0 +1,31 @@
+package counter;
+
+public class Counter {
+  int value;
+  int startValue;
+
+  public Counter() {
+
+  }
+
+  public Counter(int value) {
+    this.startValue = value;
+    this.value = value;
+  }
+
+  public void add(int number) {
+    value += number;
+  }
+
+  public void add() {
+    value++;
+  }
+
+  public int get() {
+    return value;
+  }
+
+  public void reset() {
+    value = startValue;
+  }
+}
