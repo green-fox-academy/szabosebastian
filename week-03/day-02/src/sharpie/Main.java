0c4586dd79642945977fda3ca04120af7dcd1454
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-18 13:08:22 +0100

    sharpie fixed

---
+++ week-03/day-02/src/sharpie/Main.java
@@ -0,0 +1,10 @@
+package sharpie;
+
+public class Main {
+  public static void main(String[] args) {
+    Sharpie createSharpie = new Sharpie("Blue", 1.1f, 10);
+    createSharpie.use();
+
+    System.out.println("After the use() method inkAmount: " +createSharpie.inkAmount);
+  }
+}
