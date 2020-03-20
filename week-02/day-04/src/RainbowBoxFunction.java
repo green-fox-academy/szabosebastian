674a4777113b3e79be6eb8430bb212f13f6ab73f
Szabó Sebastián <szabo.sebastian@hotmail.com>
2020-03-20 15:52:45 +0100

    RainbowBow fixed

--- week-02/day-04/src/RainbowBoxFunction.java
+++ week-02/day-04/src/RainbowBoxFunction.java
@@ -7,53 +7,51 @@ import java.util.Arrays;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RainbowBoxFunction {
-    public static void mainDraw(Graphics graphics) {
-        // Create a square drawing function that takes 3 parameters:
-        // The square size, the fill color, graphics
-        // and draws a square of that size and color to the center of the canvas.
-        // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
+  public static void mainDraw(Graphics graphics) {
+    // Create a square drawing function that takes 3 parameters:
+    // The square size, the fill color, graphics
+    // and draws a square of that size and color to the center of the canvas.
+    // Create a loop that fills the canvas with rainbow colored squares (red, orange, yellow, green, blue, indigo, violet).
 
-        int size = 320/7;
-        int x = 0;
-        int y = 0;
+    int size = WIDTH;
+    int x = 0;
+    int y = 0;
 
-        ArrayList<Color> boxColors = new ArrayList<>(Arrays.asList(Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, new Color(75,0,130),new Color(238,130,238)));
-        for (int i = 0; i < size; i++) {
-            for (Color boxColor: boxColors) {
-                squareFunction(size,x,y,boxColor,graphics);
-                x += size;
-            }
-            x = 0;
-            y += size;
-        }
+    ArrayList<Color> boxColors = new ArrayList<>(Arrays.asList(Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, new Color(75, 0, 130), new Color(238, 130, 238)));
+    for (Color boxColor : boxColors) {
+      squareFunction(size, x, y, boxColor, graphics);
+      size -= 45;
+      x += 22;
+      y += 22;
     }
+  }
 
-    public static void squareFunction(int size, int x, int y, Color boxColor, Graphics graphics){
+  public static void squareFunction(int size, int x, int y, Color boxColor, Graphics graphics) {
 
-        graphics.setColor(boxColor);
-        graphics.fillRect(x, y, size, size);
+    graphics.setColor(boxColor);
+    graphics.fillRect(x, y, size, size);
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
     }
-
-    // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
-
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-        jFrame.pack();
-    }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
+  }
 }
