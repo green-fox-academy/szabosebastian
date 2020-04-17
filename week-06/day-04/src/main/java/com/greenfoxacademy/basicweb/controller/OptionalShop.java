d579918d4be749a91c36cc2c8b7f0907df24410b
szabosebastian <szabo.sebastian@hotmail.com>
2020-04-18 00:22:44 +0200

    finished basic-web backand

--- week-06/day-04/src/main/java/com/greenfoxacademy/basicweb/controller/OptionalShop.java
+++ week-06/day-04/src/main/java/com/greenfoxacademy/basicweb/controller/OptionalShop.java
@@ -2,10 +2,8 @@ package com.greenfoxacademy.basicweb.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
